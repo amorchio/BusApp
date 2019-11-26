426762ca101f8e1c9d3a343155328d6e4b6c7f4c
amorchio <amorchio1@gmail.com>
2019-11-25 22:58:46 -0500

    added a constructor and a method to generate a random pnr

--- src/objects/Reservation.java
+++ src/objects/Reservation.java
@@ -2,20 +2,35 @@ package objects;
 
 public class Reservation {
 	
-	private int pnr = 0;
+	private String pnr;
 	private int busID;
-	private String firstName;
-	private String lastName;
+	private String first_name;
+	private String last_name;
 	private String username;
 	
-	public Reservation() {
-		pnr++;
+	public Reservation(int busID, String username) {
+		
+		pnr = generatePNR();
+		this.username = username;
 	}
 
-	public int getPnr() {
+	public String getPnr() {
 		return pnr;
 	}
 	
-	
+	private String generatePNR() {
+
+		pnr = "";
+
+		for (int i = 0; i < 3; i++) {
+			pnr += (char) ((Math.random() * 9) + 49) + "";
+		}
+		for (int i = 0; i < 3; i++) {
+			pnr += (char) ((Math.random() * 26) + 65) + "";
+		}
+
+		return pnr;
+
+	}
 
 }
